959c7e3b78589a0010d8b78e2e7ad00db39be01e
camilofg <raul_forero@epam.com>
2022-05-18 10:31:48 -0500

    Second lecture

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -20,23 +20,62 @@ namespace WinTail
 
         protected override void OnReceive(object message)
         {
-            var read = Console.ReadLine();
-            if (!string.IsNullOrEmpty(read) && String.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            if (message.Equals(StartCommand))
             {
-                // shut down the system (acquire handle to system via
-                // this actors context)
-                Context.System.Terminate();
-                return;
+                DoPrintInstructions();
+            }
+            else if (message is Messages.InputError msg)
+            {
+                _consoleWriterActor.Tell(msg);
             }
 
-            // send input to the console writer to process and print
-            // YOU NEED TO FILL IN HERE
-            _consoleWriterActor.Tell(read);
-
-            // continue reading messages from the console
-            // YOU NEED TO FILL IN HERE
-            Self.Tell("continue");
+            GetAndValidateInput();
         }
 
+        /// <summary>
+        /// Reads input from console, validates it, then signals appropriate response
+        /// (continue processing, error, success, etc.).
+        /// </summary>
+        private void GetAndValidateInput()
+        {
+            var message = Console.ReadLine();
+            if (string.IsNullOrEmpty(message))
+            {
+                Self.Tell(new Messages.NullInputError("No input received"));
+            }
+            else if (message.Equals(ExitCommand, StringComparison.OrdinalIgnoreCase)) {
+                Context.System.Terminate();
+            }
+            else
+            {
+                var valid = IsValid(message);
+                if (valid) { 
+                    _consoleWriterActor.Tell(new Messages.InputSuccess("Thank you Message was valid"));
+                    Self.Tell(new Messages.ContinueProcessing());
+                }
+                else
+                {
+                    Self.Tell(new Messages.ValidationError("Invalid: input had odd number of characters"));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Validates <see cref="message"/>.
+        /// Currently says messages are valid if contain even number of characters.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private static bool IsValid(string message)
+        {
+            return message.Length % 2 == 0;
+        }
+
+        private void DoPrintInstructions()
+        {
+            Console.WriteLine("Write whatever you want into the console!");
+            Console.WriteLine("Some entries will pass validation, and some won't...\n\n");
+            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+        }
     }
 }
