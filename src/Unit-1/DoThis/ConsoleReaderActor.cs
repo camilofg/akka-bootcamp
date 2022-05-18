0ae454f744200749113d83fe7594e50f87070a26
camilofg <raul_forero@epam.com>
2022-05-18 09:06:46 -0500

    FirstCommit

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -10,6 +10,7 @@ namespace WinTail
     class ConsoleReaderActor : UntypedActor
     {
         public const string ExitCommand = "exit";
+        public const string StartCommand = "start";
         private IActorRef _consoleWriterActor;
 
         public ConsoleReaderActor(IActorRef consoleWriterActor)
@@ -30,9 +31,11 @@ namespace WinTail
 
             // send input to the console writer to process and print
             // YOU NEED TO FILL IN HERE
+            _consoleWriterActor.Tell(read);
 
             // continue reading messages from the console
             // YOU NEED TO FILL IN HERE
+            Self.Tell("continue");
         }
 
     }
